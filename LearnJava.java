0aab3a11dcdbcebc283e09579ed70c8e7509fab8
Kenny Huang <khny258@gmail.com>
2020-09-23 19:37:19 -0400

    LearnJava

---
+++ LearnJava.java
@@ -0,0 +1,10 @@
+public class LearnJava {
+
+    public static void main(String[] args) {
+
+        String name = "Kenny";
+        System.out.println(name.toLowerCase());
+        
+    }
+    
+}
