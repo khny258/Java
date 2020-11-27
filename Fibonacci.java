ed32a6a67a0e4dd6936116628086834c07a0935d
Kenny Huang <khny258@gmail.com>
2020-11-26 21:30:10 -0500

    updated fibonacci and fizzbuzz

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,8 +1,11 @@
 public class Solution {
     public int fib(int N) {
+        // if N is smaller or equal to 1, return 1
         if (N <= 1) {
             return N;
         }
+
+        // else fibonacci sequence
         return fib(N - 1) + fib(N - 2);
     }
 }
--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,28 +1,20 @@
-import java.util.*;
-class FizzBuzz
-{
-    public static void main(String args[]) 
+class FizzBuzz {
+    public static void main (String args[])
     {
-        int n = 100;
-        
-        // loop 100 times
-        for (int i=1; i<=n; i++)
+        for (int i = 1; i <= 100; i++)
         {
-            // if divisible by 3 & 5, print 'FizzBuzz' in place of the number
-            if (i%15==0)
-                System.out.print("FizzBuzz"+" ");
-            
-            // if divisible 5, print 'Buzz' in place of the number
-            else if (i%5==0)
-                System.out.print("Buzz"+" ");
-
-            // if divisible 5, print 'Fizz' in place of the number
-            else if (i%3==0)
-                System.out.print("Fizz"+" ");
-            
-            // print the numbers
-            else
-                System.out.print(i+" ");
+            // if i is divisible by 15 print FizzBuzz
+            if (i % 15 == 0)
+                System.out.println("FizzBuzz");
+            // if i is divisible by 5 print Buzz
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            // If i is divisible by 3 print Fizz
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            // else print i
+            else   
+                System.out.println(i);
         }
     }
 }
